6f82012bb379d73c5e3afd41636be22ee9fc38e0
fre0nmeister <r.daffaag@gmail.com>
2025-12-08 13:03:16 +0700

    nambahin ulang apikey

--- firebase.ts
+++ firebase.ts
@@ -24,12 +24,12 @@ import {
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCORTMJI4PGMrX9uyjjTbb22eMqqDKFMEA',
+  authDomain: 'rn-mahasiswa.firebaseapp.com',
+  projectId: 'rn-mahasiswa',
+  storageBucket: 'rn-mahasiswa.firebasestorage.app',
+  messagingSenderId: '369493786946',
+  appId: '1:369493786946:web:c2bfa1e15bae79a0aac8db',
 };
 
 const app = initializeApp(firebaseConfig);
